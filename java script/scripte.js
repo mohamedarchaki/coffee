e134e759747826b19113a94246ee2021ff1f6b0b
mohamedarchaki <150295717+mohamedarchaki@users.noreply.github.com>
2023-11-17 22:53:54 +0100

    Add files via upload

---
+++ java script/scripte.js
@@ -0,0 +1,53 @@
+function chenge(){
+    document.getElementById("ico_ne").style.color='#eb3728';
+}
+function chengedb(){
+    document.getElementById("ico_ne").style.color='rgb(150, 150, 150)';
+}
+function cklicks1(){
+    document.getElementById('e').style.color= '#e5850c';
+}
+function cklicks2(){
+    document.getElementById('e').style.color= '#e5850c';
+    document.getElementById('f').style.color= '#e5850c';
+}
+function cklicks3(){
+    document.getElementById('e').style.color= '#e5850c';
+    document.getElementById('f').style.color= '#e5850c';
+    document.getElementById('h').style.color= '#e5850c';
+}
+function cklicks4(){
+    document.getElementById('e').style.color= '#e5850c';
+    document.getElementById('f').style.color= '#e5850c';
+    document.getElementById('h').style.color= '#e5850c';
+    document.getElementById('g').style.color= '#e5850c';
+}
+function cklicks5(){
+    document.getElementById('e').style.color= '#e5850c';
+    document.getElementById('f').style.color= '#e5850c';
+    document.getElementById('h').style.color= '#e5850c';
+    document.getElementById('g').style.color= '#e5850c';
+    document.getElementById('k').style.color= '#e5850c';
+
+}
+function dbckilcks(){
+    document.getElementById('e').style.color= 'rgb(150, 150, 150)';
+    document.getElementById('f').style.color= 'rgb(150, 150, 150)';
+    document.getElementById('h').style.color= 'rgb(150, 150, 150)';
+    document.getElementById('g').style.color= 'rgb(150, 150, 150)';
+    document.getElementById('k').style.color= 'rgb(150, 150, 150)';
+
+}
+
+function menu(){
+    document.getElementById('men').style.display='none';
+    document.getElementById('menu').style.display='block';
+    document.getElementById('me').style.display='block';
+}
+function menu2(){
+    document.getElementById('men').style.display='block';
+    document.getElementById('menu').style.display='none';
+    document.getElementById('me').style.display='none';
+
+    
+}
